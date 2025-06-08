bae6314ee1cdd2814eb0d10c5fb4ffa14072c422
Igor Mineev <igron99@mail.ru>
2025-06-08 05:20:45 +0300

    libchecker2

--- include/checker2.h
+++ include/checker2.h
@@ -39,6 +39,96 @@ enum
 
 #define checker_kill kill
 
+FILE * f_in = NULL;
+FILE * f_out = NULL;
+
+const char * _(const char *s) {
+    return s;
+}
+
+void checker_do_init(int argc, char **argv, int corr_flag, int info_flag, int tgz_flag) {
+    if (argc < 3) {
+        fatal_CF(_("arguments expected '%d'"), argc);
+    }
+
+    if (!(f_in = fopen(argv[1], "r"))) {
+        fatal_CF(_("Cannot open input file '%s'"), argv[1]);
+    }
+    if (!(f_out = fopen(argv[2], "r"))) {
+        fatal_PE(_("Cannot open output file '%s'"), argv[2]);
+    }
+}
+
+int checker_read_in_unsigned_long_long(const char *name, int eof_error_flag, unsigned long long *p_val) {
+    int r = fscanf(f_in, "%llu", p_val);
+    if (eof_error_flag && r == EOF) {
+        fatal_PE(_("Unexpected EOF while reading %s"), name);
+    }
+    return r;
+}
+
+int checker_read_in_int(const char *name, int eof_error_flag, int *p_val) {
+    int r = fscanf(f_in, "%d", p_val);
+    if (eof_error_flag && r == EOF) {
+        fatal_PE(_("Unexpected EOF while reading %s"), name);
+    }
+    return r;
+}
+
+int checker_read_out_unsigned_long_long(const char *name, int eof_error_flag, unsigned long long *p_val) {
+    int r = fscanf(f_out, "%llu", p_val);
+    if (eof_error_flag && r == EOF) {
+        fatal_PE(_("Unexpected EOF while reading %s"), name);
+    }
+    return r;
+}
+
+void checker_in_eof(void) {
+    int c;
+
+    while ((c = getc(f_in)) != EOF && isspace(c));
+    if (c != EOF) {
+        if (c < ' ') {
+            fatal_CF(_("%s: invalid control character with code %d"),
+                     "input", c);
+        } else {
+            fatal_CF(_("%s: garbage where EOF expected"), "input");
+        }
+    }
+    if (ferror(f_in)) {
+        fatal_CF(_("%s: input error"), "input");
+    }
+}
+
+void checker_in_close(void) {
+    if (!f_in) return;
+    fclose(f_in);
+    f_in = NULL;
+}
+
+void checker_out_eof(void) {
+    int c;
+
+    while ((c = getc(f_out)) != EOF && isspace(c));
+    if (c != EOF) {
+        if (c < ' ') {
+            fatal_CF(_("%s: invalid control character with code %d"),
+                     "output", c);
+        } else {
+            fatal_CF(_("%s: garbage where EOF expected"), "output");
+        }
+    }
+    if (ferror(f_out)) {
+        fatal_CF(_("%s: input error"), "output");
+    }
+}
+
+void checker_out_close(void) {
+    if (!f_out) return;
+    fclose(f_out);
+    f_out = NULL;
+}
+
 int checker_stoi(const char * str, int base, int *val) {
     char *end = NULL;
     *val = strtol(str, &end, base);
